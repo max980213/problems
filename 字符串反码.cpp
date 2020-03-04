2690374de4db2d61cba5f3551efc275b589525e0
max980213 <49967356+max980213@users.noreply.github.com>
2020-03-04 22:18:28 +0800

    Create 字符串反码.cpp

---
+++ 字符串反码.cpp
@@ -0,0 +1,38 @@
+//题目描述：定义一个字符的反码。如果这是一个小写字符，则它和字符'a’的距离与它的反码和字符'z’的距离相同；
+//如果是一个大写字符，则它和字符'A’的距离与它的反码和字符'Z’的距离相同；如果不是上面两种情况，它的反码就是它自身。
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const int maxn=1010;
+
+int main()
+{
+    char str[maxn];
+    char rev[maxn]; //临时存放反码
+    while(1)
+    {
+    gets(str);
+    if(str[0]=='!'&&str[1]=='\0')
+        return 0;
+    for(int i=0;i<strlen(str);i++)
+    {
+        if(str[i]>='a'&&str[i]<='z')
+        {
+            rev[i]=('z'-str[i]+'a');
+        }
+        else if(str[i]>='A'&&str[i]<='Z')
+        {
+            rev[i]=('Z'-str[i]+'A');
+        }
+        else
+        {
+            rev[i]=str[i];
+        }
+    }
+    puts(rev);
+    }
+}
