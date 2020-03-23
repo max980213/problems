424c1cc51eea546d1464856252b504d0620fe3fd
max980213 <49967356+max980213@users.noreply.github.com>
2020-03-23 15:58:27 +0800

    Create 打印斜着增长的特殊二维数组.cpp

---
+++ 打印斜着增长的特殊二维数组.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int A[10][10];
+    int num=1;
+    for(int i=0;i<10;i++)   //处理左上三角，外层循环是行
+    {
+        for(int j=0;j<=i;j++)
+        {
+            A[i-j][j]=num++;
+        }
+    }
+
+    for(int j=1;j<10;j++)
+    {
+        for(int i=9;i>=j;i--)
+        {
+            A[i][9-i+j]=num++;
+        }
+    }
+
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            printf("%d ",A[i][j]);
+        }
+        printf("\n");
+    }
+}
