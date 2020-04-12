9b300f798af928eea7322ee76d2f42e5135e1f70
max980213 <49967356+max980213@users.noreply.github.com>
2020-04-12 14:49:40 +0800

    Update 说反话.cpp

--- 说反话.cpp
+++ 说反话.cpp
@@ -39,3 +39,26 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+/*
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string in,out;
+    //cin>>out;
+    while(cin>>in)
+    {
+        out=in+" "+out;
+    }
+    cout<<out;
+    return 0;
+}
+*/
